72ec063cf4a9310ad9587332e03fcd195c01f3aa
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2024-02-01 17:36:30 +0000

    Create MS365Tools-Common.psm1

---
+++ Modules/MS365Tools-Common.psm1
@@ -0,0 +1,14 @@
+<#
+# ############################################################################
+# Filename: \Modules\MS365Tools-Common.psm1
+# Repository: MS365-Migration-Tools
+# Created Date: Thursday, February 1st 2024, 12:58:24 PM
+# Last Modified: Thursday, February 1st 2024, 5:11:40 PM
+# Original Author: Darnel Kumar
+# Author Github: https://github.com/Darnel-K
+# Github Org: https://github.com/ABYSS-ORG-UK/
+#
+# This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90
+# Copyright (c) 2024 Darnel Kumar - ABYSS.ORG.UK
+# ############################################################################
+#>
