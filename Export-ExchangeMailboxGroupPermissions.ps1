38ce7c2c1c342f9045656efeb135be1de7923ea0
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2024-02-09 16:44:23 +0000

    Add Comment-Based-Help section
    Add Comment-Based-Help section

--- Export-ExchangeMailboxGroupPermissions.ps1
+++ Export-ExchangeMailboxGroupPermissions.ps1
@@ -3,7 +3,7 @@
 # Filename: \Export-ExchangeMailboxGroupPermissions.ps1
 # Repository: MS365-Migration-Tools
 # Created Date: Friday, February 9th 2024, 4:27:15 PM
-# Last Modified: Friday, February 9th 2024, 4:28:52 PM
+# Last Modified: Friday, February 9th 2024, 4:42:17 PM
 # Original Author: Darnel Kumar
 # Author Github: https://github.com/Darnel-K
 # Github Org: https://github.com/ABYSS-ORG-UK/
@@ -11,4 +11,19 @@
 # This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90
 # Copyright (c) 2024 Darnel Kumar - ABYSS.ORG.UK
 # ############################################################################
+#>
+
+<#
+.SYNOPSIS
+    Exports permissions from Exchange Online
+.DESCRIPTION
+    Exports permissions from mailboxes, distribution groups & mail-enabled security groups hosted in Exchange Online
+.PARAMETER Param1
+
+.OUTPUTS
+
+.NOTES
+
+.EXAMPLE
+
 #>
