cc0a5acf7233af9f7f4ef27488aa12d1f1f7882c
Darnel Kumar <24400118+Darnel-K@users.noreply.github.com>
2024-02-09 16:32:07 +0000

    Create Export-ExchangeMailboxGroupPermissions.ps1
    This is the script to replace "Get-ExchangeMailboxDelegation.ps1"

---
+++ Export-ExchangeMailboxGroupPermissions.ps1
@@ -0,0 +1,14 @@
+<#
+# ############################################################################
+# Filename: \Export-ExchangeMailboxGroupPermissions.ps1
+# Repository: MS365-Migration-Tools
+# Created Date: Friday, February 9th 2024, 4:27:15 PM
+# Last Modified: Friday, February 9th 2024, 4:28:52 PM
+# Original Author: Darnel Kumar
+# Author Github: https://github.com/Darnel-K
+# Github Org: https://github.com/ABYSS-ORG-UK/
+#
+# This code complies with: https://gist.github.com/Darnel-K/8badda0cabdabb15359350f7af911c90
+# Copyright (c) 2024 Darnel Kumar - ABYSS.ORG.UK
+# ############################################################################
+#>
